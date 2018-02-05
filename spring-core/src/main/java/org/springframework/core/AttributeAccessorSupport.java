2bf18f14fbd8fa8835948a728075761df31ba1bc
汤旗 <tangqihlw@bbktel.com>
2018-02-05 14:45:43 +0800

    代码优化

--- spring-core/src/main/java/org/springframework/core/AttributeAccessorSupport.java
+++ spring-core/src/main/java/org/springframework/core/AttributeAccessorSupport.java
@@ -25,7 +25,7 @@ import org.springframework.util.Assert;
 /**
  * Support class for {@link AttributeAccessor AttributeAccessors}, providing
  * a base implementation of all methods. To be extended by subclasses.
- *
+ * <p>
  * <p>{@link Serializable} if subclasses and all attribute values are {@link Serializable}.
  *
  * @author Rob Harrop
@@ -35,73 +35,75 @@ import org.springframework.util.Assert;
 @SuppressWarnings("serial")
 public abstract class AttributeAccessorSupport implements AttributeAccessor, Serializable {
 
-	/** Map with String keys and Object values */
-	private final Map<String, Object> attributes = new LinkedHashMap<String, Object>(0);
+    /**
+     * Map with String keys and Object values
+     */
+    private final Map<String, Object> attributes = new LinkedHashMap<String, Object>(0);
 
 
-	@Override
-	public void setAttribute(String name, Object value) {
-		Assert.notNull(name, "Name must not be null");
-		if (value != null) {
-			this.attributes.put(name, value);
-		}
-		else {
-			removeAttribute(name);
-		}
-	}
+    @Override
+    public void setAttribute(String name, Object value) {
+        Assert.notNull(name, "Name must not be null");
+        if (value != null) {
+            this.attributes.put(name, value);
+        } else {
+            removeAttribute(name);
+        }
+    }
 
-	@Override
-	public Object getAttribute(String name) {
-		Assert.notNull(name, "Name must not be null");
-		return this.attributes.get(name);
-	}
+    @Override
+    public Object getAttribute(String name) {
+        Assert.notNull(name, "Name must not be null");
+        return this.attributes.get(name);
+    }
 
-	@Override
-	public Object removeAttribute(String name) {
-		Assert.notNull(name, "Name must not be null");
-		return this.attributes.remove(name);
-	}
+    @Override
+    public Object removeAttribute(String name) {
+        Assert.notNull(name, "Name must not be null");
+        return this.attributes.remove(name);
+    }
 
-	@Override
-	public boolean hasAttribute(String name) {
-		Assert.notNull(name, "Name must not be null");
-		return this.attributes.containsKey(name);
-	}
+    @Override
+    public boolean hasAttribute(String name) {
+        Assert.notNull(name, "Name must not be null");
+        return this.attributes.containsKey(name);
+    }
 
-	@Override
-	public String[] attributeNames() {
-		return this.attributes.keySet().toArray(new String[this.attributes.size()]);
-	}
-
-
-	/**
-	 * Copy the attributes from the supplied AttributeAccessor to this accessor.
-	 * @param source the AttributeAccessor to copy from
-	 */
-	protected void copyAttributesFrom(AttributeAccessor source) {
-		Assert.notNull(source, "Source must not be null");
-		String[] attributeNames = source.attributeNames();
-		for (String attributeName : attributeNames) {
-			setAttribute(attributeName, source.getAttribute(attributeName));
-		}
-	}
+    @Override
+    public String[] attributeNames() {
+        return this.attributes.keySet().toArray(new String[this.attributes.size()]);
+    }
 
 
-	@Override
-	public boolean equals(Object other) {
-		if (this == other) {
-			return true;
-		}
-		if (!(other instanceof AttributeAccessorSupport)) {
-			return false;
-		}
-		AttributeAccessorSupport that = (AttributeAccessorSupport) other;
-		return this.attributes.equals(that.attributes);
-	}
+    /**
+     * Copy the attributes from the supplied AttributeAccessor to this accessor.
+     *
+     * @param source the AttributeAccessor to copy from
+     */
+    protected void copyAttributesFrom(AttributeAccessor source) {
+        Assert.notNull(source, "Source must not be null");
+        String[] attributeNames = source.attributeNames();
+        for (String attributeName : attributeNames) {
+            setAttribute(attributeName, source.getAttribute(attributeName));
+        }
+    }
 
-	@Override
-	public int hashCode() {
-		return this.attributes.hashCode();
-	}
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!( other instanceof AttributeAccessorSupport )) {
+            return false;
+        }
+        AttributeAccessorSupport that = (AttributeAccessorSupport)other;
+        return this.attributes.equals(that.attributes);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.attributes.hashCode();
+    }
 
 }
