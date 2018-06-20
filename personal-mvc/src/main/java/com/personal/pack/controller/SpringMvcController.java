b6e6a429232ce6e867f5f9d3d65be0e857280fc4
汤旗 <tangqihlw@bbktel.com>
2018-06-20 10:58:57 +0800

    Spring Bean生存周期测试学习 2018-06-20

---
+++ personal-mvc/src/main/java/com/personal/pack/controller/SpringMvcController.java
@@ -0,0 +1,27 @@
+package com.personal.pack.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * 类描述：SpringMVC测试控制器
+ *
+ * @author 汤旗
+ * @date 2018-06-20
+ */
+@RequestMapping("/mvc")
+public class SpringMvcController {
+
+    @RequestMapping(value = "/test", method = {RequestMethod.GET})
+    @ResponseBody
+    public String test(@RequestParam(value = "id", required = true) Integer id) {
+        return "mvctest";
+    }
+
+    @RequestMapping(value = "/view", method = {RequestMethod.GET})
+    public ModelAndView view(@RequestParam(value = "id", required = false) Integer id) {
+        ModelAndView view = new ModelAndView();
+        view.setViewName("mvc");
+        return view;
+    }
+}
---
+++ personal-mvc/src/main/webapp/views/jsp/mvc.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/6/20
+  Time: 10:47
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+
+<html>
+    <head>
+        <title>SpringMVC学习</title>
+    </head>
+    <body>
+        <spring:eval expression="contentModel.money"></spring:eval><br/>
+    </body>
+</html>
